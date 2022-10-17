717b012fcb1fb51a624a96c0562581fd6fa8522a
Aleksandra <Sandra_74_95@mail.ru>
2022-10-17 22:24:44 +0500

    первый этап написания программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿using static System.Console;
 Clear();
+
+Write("Введите слова или числа через пробел: ");
+string[] startArray = ReadLine().Split(" ");
+WriteLine($"Начальный массив = {String.Join (" ", startArray)}");
