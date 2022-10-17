3eff62183ff583f3efa44dc84cc636760320a317
Aleksandra <Sandra_74_95@mail.ru>
2022-10-17 22:28:49 +0500

    второй этап написания программы

--- Program.cs
+++ Program.cs
@@ -4,3 +4,19 @@ Clear();
 Write("Введите слова или числа через пробел: ");
 string[] startArray = ReadLine().Split(" ");
 WriteLine($"Начальный массив = {String.Join (" ", startArray)}");
+
+string[] resultArray = new string[startArray.Length];
+void SecondArrayWithIF(string[] startArray, string[] resultArray)
+{
+    int count = 0;
+    for (int i = 0; i < startArray.Length; i++)
+    {
+    if(startArray[i].Length <= 3)
+        {
+        resultArray[count] = startArray[i];
+        count++;
+        }
+    }
+}
+SecondArrayWithIF(startArray,resultArray);
+WriteLine($"Итоговый массив = {String.Join (" ", resultArray)}");
